181826862b24fa43973d1b4b7fc88c9575043e2f
chango <kimtothechang@gmail.com>
2022-01-01 22:14:43 +0900

    🐛  Fix all my trash code

--- js/app.js
+++ js/app.js
@@ -3,24 +3,12 @@ const modal = document.querySelector(".modal");
 const btnSubs = document.querySelector(".btn-subs");
 const btnLoveChnago = document.querySelector(".love-chango");
 
-btnSubs.addEventListener("click", () => {
-  let winX = window.pageXOffset;
-  let winY = window.pageYOffset;
-  modal.classList.toggle("show");
-  console.log(winX, winY);
-  modal.style.top = `${winY}px`;
+// modal.style.display = "none";
 
-  if (modal.classList.contains("show")) {
-    // body.style.overflow = "hidden";
-  }
+btnSubs.addEventListener("click", () => {
+  modal.classList.toggle("show");
 });
 
 btnLoveChnago.addEventListener("click", (event) => {
-  if (event.target === btnLoveChnago) {
-    modal.classList.toggle("show");
-
-    if (!modal.classList.contains("show")) {
-      // body.style.overflow = "auto";
-    }
-  }
+  modal.classList.toggle("show");
 });
