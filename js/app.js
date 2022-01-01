409e5ce7849d0988eb05539a581d4a6dc1da0f9e
chango <kimtothechang@gmail.com>
2022-01-02 01:53:10 +0900

    :memo: add scroll-top function

--- js/app.js
+++ js/app.js
@@ -12,3 +12,29 @@ btnSubs.addEventListener("click", () => {
 btnLoveChnago.addEventListener("click", (event) => {
   modal.classList.toggle("show");
 });
+
+const headerNav = document.querySelector(".menu-modal");
+const btnMenu = document.querySelector(".menu-btn");
+const btnBack = document.querySelector(".go-back");
+
+btnMenu.addEventListener("click", () => {
+  headerNav.classList.toggle("show");
+});
+
+btnBack.addEventListener("click", () => {
+  headerNav.classList.toggle("show");
+});
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  toTop.classList.add("show");
+});
+
+/* Jquery */
+
+let $target = $(".to-top");
+$target.on("click", function () {
+  $("html, body").animate({ scrollTop: 0 }, 500);
+  return false;
+});
